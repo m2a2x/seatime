4be0cba3e0fca9c15015103f369aeb67326999b1
m2a2x <mojpo4tovik@gmail.com>
2017-07-19 02:22:10 +0300

    single user update

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -14,32 +14,12 @@ const User = mongoose.model('User');
  */
 
 module.exports = new FacebookStrategy({
-    clientID: config.facebook.clientID,
-    clientSecret: config.facebook.clientSecret,
-    callbackURL: config.facebook.callbackURL,
-    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
-  },
-  function (accessToken, refreshToken, profile, done) {
-    const options = {
-      criteria: { 'facebook.id': profile.id }
-    };
-    User.load(options, function (err, user) {
-      if (err) return done(err);
-      if (!user) {
-        user = new User({
-          name: profile.displayName,
-          email: profile.emails && profile.emails[0].value || '',
-          username: profile.username,
-          provider: 'facebook',
-          facebook: profile._json
-        });
-        user.save(function (err) {
-          if (err) console.log(err);
-          return done(err, user);
-        });
-      } else {
-        return done(err, user);
-      }
-    });
-  }
+        clientID: config.facebook.clientID,
+        clientSecret: config.facebook.clientSecret,
+        callbackURL: config.facebook.callbackURL,
+        profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
+    },
+    function (accessToken, refreshToken, profile, done) {
+        User.loginUser('facebook', profile, profile.emails[0].value, done);
+    }
 );
--- config/passport/google.js
+++ config/passport/google.js
@@ -19,26 +19,6 @@ module.exports = new GoogleStrategy({
     callbackURL: config.google.callbackURL
   },
   function (accessToken, refreshToken, profile, done) {
-    const options = {
-      criteria: { 'google.id': profile.id }
-    };
-    User.load(options, function (err, user) {
-      if (err) return done(err);
-      if (!user) {
-        user = new User({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          username: profile.username,
-          provider: 'google',
-          google: profile._json
-        });
-        user.save(function (err) {
-          if (err) console.log(err);
-          return done(err, user);
-        });
-      } else {
-        return done(err, user);
-      }
-    });
+      User.loginUser('google', profile, profile.emails[0].value, done);
   }
 );
