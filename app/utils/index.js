128af0f81b40a4c117e8746c446756e997895d37
m2a2x <mojpo4tovik@gmail.com>
2017-07-29 13:24:05 +0300

    wear api, rating

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,17 +1,21 @@
 const moment = require('moment');
+const _ = require('lodash');
 
 module.exports = {
-    respond,
+    parseStringToIds,
+    respondError,
     respondOrRedirect,
     getToday,
     daysToTime,
     time
 };
 
-function respond(res, tpl, obj, status) {
+function respondError(res, status) {
     res.format({
-        html: () => res.render(tpl, obj),
         json: () => {
+            let obj = {
+                isSuccesful: false
+            };
             if (status) return res.status(status).json(obj);
             res.json(obj);
         }
@@ -38,4 +42,14 @@ function time(time) {
 
 function daysToTime(days) {
     return days * 24 * 60 * 60;
+}
+
+function parseStringToIds(strIds) {
+    return _.reduce(strIds.split(','), function (result, id) {
+        id = _.parseInt(id.trim());
+        if (id) {
+            result.push(id);
+        }
+        return result;
+    }, []);
 }
